f997ab21cd350f561536010ff9cb6315cd800d38
Sam Thompson <electronic62794@yahoo.com>
2019-08-07 23:07:58 -0700

    created a back end with express, and got it talking to the front end

---
+++ tournament-maker/tournament-maker-backend/routes/users.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.json([{
+    id:"1",
+    username:"billy"
+  },
+{
+  id: "2",
+  username: "bobby"
+}])
+});
+
+module.exports = router;
